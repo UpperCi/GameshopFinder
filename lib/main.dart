a13d7bf25e247b33d92d5c04b87cabddf14897f7
upperc <daniverschoor@outlook.com>
2023-06-13 07:10:26 +0200

    refactor

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
 import 'page_router.dart';
+import 'navigation_bar.dart';
+import 'page_display.dart';
 
 void main() {
   runApp(const Stores());
@@ -18,31 +20,29 @@ class Stores extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.amber),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const App(),
     );
   }
 }
 
-class MyHomePage extends StatelessWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
+class App extends StatelessWidget {
+  const App({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(title),
-      ),
-      body: const Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            PageRouter(),
-          ],
-        ),
-      ),
-    );
+    return ChangeNotifierProvider(
+        create: (context) => PageRouterState(),
+        child: const Scaffold(
+            bottomNavigationBar: NavBar(),
+            body: SafeArea(child: PageDisplay())));
+  }
+}
+
+class AppState extends ChangeNotifier {
+  var pageIndex = 0;
+
+  void setCurrentPage(int newIndex) {
+    pageIndex = newIndex;
+    notifyListeners();
   }
 }
